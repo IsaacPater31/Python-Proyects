ead657f42c4d45465b1f50b4c83754db90fc0b8a
Isaacpat31 <isaacpat2005@gmail.com>
2025-08-30 20:34:33 -0500

    testing ifs and fors

--- holamundo.py
+++ holamundo.py
@@ -14,4 +14,34 @@ if hora<12:
 elif hora>=12 and hora<19:
     print("Buenas tardes")  
 else:
-    print("Buenas noches")      
+    print("Buenas noches")
+    
+
+edad=70
+
+if edad<=17:
+    print("Eres menor de edad")
+elif edad>17 and edad<=30:
+    print("Eres joven")
+elif edad>30 and edad<=60:
+    print("Eres adulto")
+else:
+    print("Eres un adulto mayor")
+
+for i in range(10):
+    print("El número es :",i+1,"\n",i+1)
+
+class Persona:
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+
+    def saludar(self):
+        print("Hola, mi nombre es ",self.nombre," y tengo ",self.edad," años")
+
+
+persona1=Persona("Juan",25)
+persona1.saludar()
+
+persona2=Persona("María",30)
+persona2.saludar()
